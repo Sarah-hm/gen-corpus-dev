a04a52508a6090502499a04f0373a25cc049f7cf
Sarah Hontoy-Major <sarah.hm@hotmail.ca>
2023-02-01 15:53:58 -0500

    made the header a lil more functional

--- js/script.js
+++ js/script.js
@@ -14,16 +14,28 @@ window.onload = function(){
 
         if (window.pageYOffset > 5){
         header.style.height = "10vh"
+        console.log("greater than 5")
         headerAbstract.style.color = blackTransparent;
         headerCourseTitle.style.color = blackTransparent;
         setTimeout(displayNone (headerAbstract), 1000)
+        setTimeout(displayNone(headerCourseTitle),1000)
         } else {
+        console.log("smoller than 5")
+        displayBlock(headerAbstract);
         header.style.height = "50vh"
-        headerAbstract.removeClass("displayNone");
+       // headerAbstract.removeClass("displayNone");
         headerAbstract.style.color = black;
         headerCourseTitle.style.color = black;
+     
         }
 
+        function displayNone(obj){
+            obj.classList.add("displayNone")
+        }
+
+        function displayBlock(obj){
+            obj.classList.remove("displayNone");
+        }
     });
 
 }
